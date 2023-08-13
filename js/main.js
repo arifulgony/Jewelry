b42ba61d56b90e2a3371bf1ee46535aeedb1d0b4
arifulgony <arifulgony@gmail.com>
2023-08-13 22:04:20 +0600

    all section design done

--- js/main.js
+++ js/main.js
@@ -17,3 +17,57 @@ let interval = setInterval(function() {
 // logo slider
 var copy = document.querySelector(".logos-slide").cloneNode(true);
       document.querySelector(".logos").appendChild(copy);
+
+//----------------------------------------------------------------------------
+
+
+  // slider 2  -- desktop view
+
+  const productCard = document.querySelector('.productCard');
+  const prevButton = document.getElementById('prev-button');
+  const nextButton = document.getElementById('next-button');
+  const slideWidth = 20; // Percentage width of each slide
+  let currentSlide = 0;
+
+  nextButton.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % 3;
+    productCard.style.transform = `translateX(-${currentSlide * slideWidth}%)`;
+  });
+
+  prevButton.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + 3) % 3;
+    productCard.style.transform = `translateX(-${currentSlide * slideWidth}%)`;
+  });
+
+// slider 2 -- mobile view 
+
+const slider3 = document.querySelector('.productCard');
+const paginationDots = document.querySelectorAll('.pagination-dot');
+let currentSlideMobileView = 0;
+
+function goToSlide(slideIndex) {
+  currentSlideMobileView = slideIndex;
+  slider3.style.transform = `translateX(-${currentSlideMobileView * 35}%)`;
+
+  paginationDots.forEach((dot, index) => {
+    dot.classList.toggle('active', index === currentSlideMobileView);
+  });
+}
+
+
+
+// sliderAccesories slide 
+
+const sliderAccessories = document.querySelector('.accessories-slide');
+const paginationDotsAccessories = document.querySelectorAll('.pagination-dot-accessories');
+let currentSlideAccessoriesMobileView = 0;
+
+function goToAccessoriesSlide(slideIndex) {
+  currentSlideAccessoriesMobileView = slideIndex;
+  sliderAccessories.style.transform = `translateX(-${currentSlideAccessoriesMobileView * 35}%)`;
+
+  paginationDotsAccessories.forEach((dot, index) => {
+    dot.classList.toggle('active', index === currentSlideAccessoriesMobileView);
+  });
+}
+
