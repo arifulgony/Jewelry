28decf23dac1fb188ad54c2921f8017883cf1f2d
arifulgony <arifulgony@gmail.com>
2023-08-15 22:41:03 +0600

    project done

--- js/main.js
+++ js/main.js
@@ -18,10 +18,9 @@ let interval = setInterval(function() {
 var copy = document.querySelector(".logos-slide").cloneNode(true);
       document.querySelector(".logos").appendChild(copy);
 
-//----------------------------------------------------------------------------
 
 
-  // slider 2  -- desktop view
+  // product card slider  -- desktop view
 
   const productCard = document.querySelector('.productCard');
   const prevButton = document.getElementById('prev-button');
@@ -39,7 +38,7 @@ var copy = document.querySelector(".logos-slide").cloneNode(true);
     productCard.style.transform = `translateX(-${currentSlide * slideWidth}%)`;
   });
 
-// slider 2 -- mobile view 
+// product card slider -- mobile view 
 
 const slider3 = document.querySelector('.productCard');
 const paginationDots = document.querySelectorAll('.pagination-dot');
@@ -53,8 +52,6 @@ function goToSlide(slideIndex) {
     dot.classList.toggle('active', index === currentSlideMobileView);
   });
 }
-
-
 
 // sliderAccesories slide 
 
@@ -71,3 +68,118 @@ function goToAccessoriesSlide(slideIndex) {
   });
 }
 
+//table data
+function tableData() {
+  var info = document.getElementById("info");
+  info.innerHTML = ` 
+  <tr>
+   <td class="ferure-hading"><p>lorem</p></td>
+   <td class="table-heding"><h3>lorem 100</h3> <p>Jewelry Photography Light Box and Software</p></td>
+   <td class="table-heding"><h3>lorem 80</h3> <p>Jewelry Photography Solution – Still, 360, 3D and Video</p></td>
+   <td class="table-heding d-none"><h3>lorem 80 Air</h3><p>All in One Jewelry, Loose Diamond and Gemstone Photography System – Still, 360, 3D and Video</p></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Software Controlled Camera, Light and Turntable Settings</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Multiple Camera Control</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Real Time Preview</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Batch Editing, Processing & Saving</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Batch Editing, Processing & Saving</p></td>
+   <td class="feature-data"><p>Dimmable LEDs
+       50,000 hour lifespan
+       95+ CRI
+       11,400 Lumens</p></td>
+   <td class="feature-data"><p>Dimmable LEDs
+       50,000 hour lifespan
+       95+ CRI
+       11,400 Lumens</p></td>
+   <td class="feature-data d-none"><p>Dimmable LEDs
+       50,000 hour lifespan
+       95+ CRI
+       11,400 Lumens</p></td>
+  </tr>
+
+`;
+}
+
+function tablerowData() {
+  var info = document.getElementById("tableData");
+  info.innerHTML = ` 
+   <td class="table-feature"><p>Batch Editing, Processing & Saving</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+`;
+}
+
+
+function tableDataleft() {
+  var info = document.getElementById("info");
+  info.innerHTML = ` 
+  <tr>
+   <td class="ferure-hading"><p>Features</p></td>
+   <td class="table-heding"><h3>PhotoBench 100</h3> <p>Jewelry Photography Light Box and Software</p></td>
+   <td class="table-heding"><h3>PhotoBench 80</h3> <p>Jewelry Photography Solution – Still, 360, 3D and Video</p></td>
+   <td class="table-heding d-none"><h3>PhotoBench 80 Air</h3><p>All in One Jewelry, Loose Diamond and Gemstone Photography System – Still, 360, 3D and Video</p></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Software Controlled Camera, Light and Turntable Settings</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Multiple Camera Control</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Real Time Preview</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Batch Editing, Processing & Saving</p></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img"><img src="./img/icone/tikmark.png" alt=""></td>
+   <td class="table-img d-none"><img src="./img/icone/tikmark.png" alt=""></td>
+  </tr>
+  <tr>
+   <td class="table-feature"><p>Batch Editing, Processing & Saving</p></td>
+   <td class="feature-data"><p>Dimmable LEDs
+       50,000 hour lifespan
+       95+ CRI
+       11,400 Lumens</p></td>
+   <td class="feature-data"><p>Dimmable LEDs
+       50,000 hour lifespan
+       95+ CRI
+       11,400 Lumens</p></td>
+   <td class="feature-data d-none"><p>Dimmable LEDs
+       50,000 hour lifespan
+       95+ CRI
+       11,400 Lumens</p></td>
+  </tr>
+
+`;
+}
